bbb7377b8fec7a50ca9d31f81ce41bd538387921
meeta haldar <meetahaldar1001@gmail.com>
2021-12-01 17:18:58 +0530

    to do app completed

---
+++ script.js
@@ -0,0 +1,42 @@
+let form = document.getElementById("form");
+let input=document.getElementById("myInput");
+let theList=document.getElementById("theList");
+let btn =document.getElementById("addItem");
+
+btn.disabled = true;
+
+
+input.addEventListener("change", stateHandle);
+
+function stateHandle() {
+    if(input.value==="") {
+        btn.disabled = true;
+       
+    } else {
+        btn.disabled = false;
+    
+    }
+}
+
+
+
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    createItem(myInput.value);
+}
+)
+function createItem(x)
+{
+    let outHTML =`<li>${x}  <button class="btn" onclick ="deleteItem(this)"> <i class="fas fa-trash-alt m-2"></i> </button></li>`;
+    theList.insertAdjacentHTML("beforeend",outHTML);
+    myInput.value='';
+    myInput.focus()
+    btn.disabled = true;
+}
+
+
+function deleteItem(y){
+y.parentElement.remove();
+
+}
